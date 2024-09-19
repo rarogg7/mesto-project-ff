ad4c7b09456a4c17fe7a343fc38ffa70ccebb082
Vladimir Lavrichenko <resilience77@yandex.ru>
2024-09-20 00:21:48 +0500

    Ver. 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
+// import {initialCards} from "./cards";
+const log = console.log;
+
 // @todo: Темплейт карточки
+
+const templateCard = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
 
+const placeCardList = document.querySelector(".places__list");
+const avatarProfile = document.querySelector(".profile__image");
+const profileTitle = document.querySelector(".profile__title");
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileDescription = document.querySelector(".profile__description");
+const avatarLinkInput = document.getElementById("avatar-link");
+const cardButtonDelete = document.querySelector(".card__delete-button");
+
 // @todo: Функция создания карточки
+
+initialCards.forEach((item) => {
+  const cardElement = templateCard.cloneNode(true);
+  cardElement.querySelector(".card__title").textContent = item.name;
+  cardElement.querySelector(".card__image").src = item.link;
+  cardElement.querySelector(".card__image").alt = item.name;
+  placeCardList.append(cardElement);
+});
 
 // @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+document.querySelector("body").onclick = (ev) => {
+  if (ev.target.className != "card__delete-button") return;
+  ev.target.closest(".card").remove();
+  // item.remove();
+};
