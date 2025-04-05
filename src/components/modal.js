784d2afd30c39b6ea207afe48654e3d3bdce545d
Vladimir Lavrichenko <resilience77@yandex.ru>
2025-04-05 11:56:28 +0300

    внес правки 2.0

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,19 +1,21 @@
 // Функция открытия попапа
-export function openModal(popupElement) {
+function openModal(popupElement) {
   popupElement.classList.add("popup_is-opened");
   document.addEventListener("keydown", closeKeyEscape);
 }
 
 // Функция закрытия попапа
-export function closeModal(popupElement) {
-  popupElement.classList.remove("popup_is-opened"); 
+function closeModal(popupElement) {
+  popupElement.classList.remove("popup_is-opened");
   document.removeEventListener("keydown", closeKeyEscape);
 }
 
 // Функция закрытия попапа по нажатию на ESC
-export function closeKeyEscape(event) {
-  if (event.key === "Escape") {
-    const popupIsOpened = document.querySelector("popup_is-opened");
+function closeKeyEscape(evt) {
+  if (evt.key === "Escape") {
+    const popupIsOpened = document.querySelector(".popup_is-opened");
     closeModal(popupIsOpened);
   }
 }
+
+export {openModal, closeModal}
